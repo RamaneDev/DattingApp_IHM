d7e9942c898614f9970845ab5ded7b2be406af19
ABDEL <ABDEL@outlook.fr>
2020-03-25 13:14:15 +0100

    creation of AuthService an Injection In NavComponent

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+   this.authService.login(this.model).subscribe(next => {
+     console.log('login succeful');
+   }, error => {
+     console.log('login fail !');
+   });
   }
 
 }
