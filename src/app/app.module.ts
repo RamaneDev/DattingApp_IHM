6ea624767e3aa4a1162d74aa0b16ac6d0805b584
ABDEL <ABDEL@outlook.fr>
2020-03-26 23:24:58 +0100

    Remove Value component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
@@ -15,7 +13,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
       RegisterComponent,
       HomeComponent
