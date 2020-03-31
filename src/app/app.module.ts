e573e6997a1ed0c19f194e82563bb4ac8335715e
ABDEL <ABDEL@outlook.fr>
2020-03-31 17:22:01 +0200

    Add (appRoutes) & routes.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -11,9 +12,10 @@ import { HomeComponent } from './home/home.component';
 import { ErrorIterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
-
-
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MembersListComponent } from './members-list/members-list.component';
+import { appRoutes } from './routes';
 
 
 @NgModule({
@@ -21,14 +23,19 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
       AppComponent,
       NavComponent,
       RegisterComponent,
-      HomeComponent
+      HomeComponent,
+      ListsComponent,
+      MessagesComponent,
+      MembersListComponent,
+      MembersListComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       ErrorIterceptorProvider
