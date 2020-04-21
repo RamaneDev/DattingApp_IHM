ed5ac8d7a13689cab42d08d6f65e2e2d8b6a108b
ABDEL <ABDEL@outlook.fr>
2020-04-21 21:34:44 +0200

    Add pagination model

---
+++ src/app/_models/pagination.ts
@@ -0,0 +1,12 @@
+export interface Pagination {
+    itemsPerPage: number;
+    totalItems: number;
+    totalPages: number;
+    currentPage: number;
+}
+
+export class PaginatedResult<T> {
+    result: T;
+    pagination: Pagination;
+}
+
