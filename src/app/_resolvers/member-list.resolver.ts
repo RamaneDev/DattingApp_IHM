d3fb47b0788a67851f88da5475d39547f8900951
ABDEL <ABDEL@outlook.fr>
2020-04-21 21:36:05 +0200

    update user.service.ts for pagination

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -5,16 +5,20 @@ import { Observable, of } from 'rxjs';
 import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { catchError } from 'rxjs/operators';
+import { PaginatedResult } from '../_models/pagination';
 
 
 
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+  pageNumber = 1;
+  pageSize = 5;
   constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers().pipe(
+    console.log('resolver');
+    return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
         catchError(error => {
             this.alertify.error('Problem retrieving List');
             this.router.navigate(['/home']);
--- src/app/members/members-list/members-list.component.ts
+++ src/app/members/members-list/members-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-members-list',
@@ -12,12 +13,29 @@ import { ActivatedRoute } from '@angular/router';
 export class MembersListComponent implements OnInit {
 
   users: User[];
+  pagination: Pagination;
   constructor(private userService: UserService, private alertifyService: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
   }
 
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+        .subscribe((resp: PaginatedResult<User[]>) => {
+      this.users = resp.result;
+      this.pagination = resp.pagination;
+    }, error => {
+      this.alertifyService.error(error);
+    });
+  }
 }
